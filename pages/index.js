8faa3c0c1653c211f444b303daf0a3bca715e529
Maria Gromyko <mazvereva91@mail.ru>
2022-06-03 22:44:24 +0300

    feat: добавление js к форме

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,34 @@
 const buttonOpen = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__content');
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector('.popup__text_type_name');
+let jobInput = formElement.querySelector('.popup__text_type_description');
+// Находим имя и работу в профили, которые нужно будет изменить 
+let newName = document.querySelector('.profile__title');
+let newJob = document.querySelector('.profile__subtitle');
 
+//Попап открыт, значения в поле формы = исходному имени и работе
 buttonOpen.addEventListener('click', function () {
     popup.classList.remove('popup_hidden');
+    nameInput.value = newName.textContent;
+    jobInput.value = newJob.textContent;
 })
 
 popupCloseButton.addEventListener('click', function () {
     popup.classList.add('popup_hidden');
 })
 
-popup.addEventListener('click', function (e) {
-    if (e.target === e.currentTarget) {
-        popup.classList.add('popup_hidden');
-    }
-});
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__content');
-// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__text_type_name');
-// Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.popup__text_type_description');
- // Воспользуйтесь инструментом .querySelector()
-
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault()
-        
-    ; // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+function formSubmitHandler(evt) {
+    evt.preventDefault();// Эта строчка отменяет стандартную отправку формы.
 
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+    newName.textContent = nameInput.value; //новые элементы полей
+    newJob.textContent = jobInput.value;
+    popup.classList.add('popup_hidden');
 }
 
 // Прикрепляем обработчик к форме:
