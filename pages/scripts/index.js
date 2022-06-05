3c2d3b68dbe9a42a023abc534b9cf9f337ff7376
Maria Gromyko <mazvereva91@mail.ru>
2022-06-05 16:12:40 +0300

    fix: исправление замечаний 1го ревью

--- pages/scripts/index.js
+++ pages/scripts/index.js
@@ -11,25 +11,30 @@ let jobInput = formElement.querySelector('.popup__text_type_description');
 let newName = document.querySelector('.profile__title');
 let newJob = document.querySelector('.profile__subtitle');
 
-//Попап открыт, значения в поле формы = исходному имени и работе
-buttonOpen.addEventListener('click', function () {
+//объявим функция открытия попап
+function popupOpen() {
     popup.classList.remove('popup_hidden');
     nameInput.value = newName.textContent;
     jobInput.value = newJob.textContent;
-})
+}
 
-//Попап закрыт
-popupCloseButton.addEventListener('click', function () {
+// объявим функцю закрытия попап
+function popupClose() {
     popup.classList.add('popup_hidden');
-})
+}
+
+//Слушатель действий. Попап открыт, значения в поле формы = исходному имени и работе
+buttonOpen.addEventListener('click', popupOpen);
+
+//Слушатель действий. Попап закрыт
+popupCloseButton.addEventListener('click', popupClose);
 
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 function formSubmitHandler(evt) {
     evt.preventDefault();// Эта строчка отменяет стандартную отправку формы.
-
     newName.textContent = nameInput.value; //новые элементы полей
     newJob.textContent = jobInput.value;
-    popup.classList.add('popup_hidden'); //закрытие попапа по "Сохранению"
+    popupClose();  //закрытие попапа по "Сохранению"
 }
 
 // Прикрепляем обработчик к форме:
