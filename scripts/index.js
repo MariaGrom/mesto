2e8d4244d6d8ad635dfa33c3b2c43a50016200bb
Maria Gromyko <mazvereva91@mail.ru>
2022-06-19 18:47:02 +0300

    feat: project 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 
 // Попал для изменения профиля пользователя
-const buttonOpen = document.querySelector('.profile__edit-button');//Находим кнопки 
+const buttonOpen = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
-let formElement = document.querySelector('.popup__content');// Находим форму в DOM
-let nameInput = formElement.querySelector('.popup__text_type_name');// Находим поля формы в DOM
-let jobInput = formElement.querySelector('.popup__text_type_description');// Находим имя и работу в профили, которые нужно будет изменить 
+let formElement = document.querySelector('.popup__content');
+let nameInput = formElement.querySelector('.popup__text_type_name');
+let jobInput = formElement.querySelector('.popup__text_type_description');
 let newName = document.querySelector('.profile__title');
 let newJob = document.querySelector('.profile__subtitle');
 
@@ -29,10 +29,10 @@ popupCloseButton.addEventListener('click', popupClose);
 
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 function formSubmitHandler(evt) {
-    evt.preventDefault();// Эта строчка отменяет стандартную отправку формы.
+    evt.preventDefault();
     newName.textContent = nameInput.value; //новые элементы полей
     newJob.textContent = jobInput.value;
-    popupClose();  //закрытие попапа по "Сохранению"
+    popupClose();
 }
 
 // Прикрепляем обработчик к форме:он будет следить за событием “submit” - «отправка»
@@ -41,7 +41,7 @@ formElement.addEventListener('submit', formSubmitHandler);
 // Добавление попапа для Нового места
 const buttonAddOpen = document.querySelector('.profile__add-button');
 const popupPlace = document.querySelector('.popup_place');
-const popupPlaceCloseButton = document.querySelector('.popup-place__close-button');
+const popupPlaceCloseButton = popupPlace.querySelector('.popup__close-button');
 // Добавление попапа для Демонстрации фото
 const buttonOpenPhoto = document.querySelector('.elements__photo');
 const popupPhoto = document.querySelector('.popup_photo');
@@ -54,7 +54,6 @@ function popupPlaceOpen() {
 
 //Слушатель действий. Попап места открыт
 buttonAddOpen.addEventListener('click', popupPlaceOpen);
-
 
 // объявим функцю закрытия попапа места
 function popupPlaceClose() {
@@ -79,42 +78,40 @@ function popupPhotoClose() {
 buttonClosePhoto.addEventListener('click', popupPhotoClose);
 
 // Добавление карточек
-const itemTemplate = document.querySelector(".item__template").content;
-const list = document.querySelector(".elements__items");
-const form = document.querySelector(".popup__content_place");
-const formButton = document.querySelector(".popup__submit-button"); // нужна ли она мне?
-const formInputName = document.querySelector(".popup-place__text_type_name");
-const formInputLink = document.querySelector(".popup-place__text_type_link");
-const buttonLike = document.querySelector(".elements__like");
+const itemTemplate = document.querySelector('.item__template').content;
+const list = document.querySelector('.elements__items');
+const form = document.querySelector('.popup__content_place');
+const formInputName = popupPlace.querySelector('.popup__text_type_name');
+const formInputLink = popupPlace.querySelector('.popup__text_type_link');
+const buttonLike = document.querySelector('.elements__like');
 const popupPhotoTitle = document.querySelector('.popup__caption');
 const popupPhotoImg = document.querySelector('.popup__photo');
 
 //функция добавления карточки
 function addCard(name, link) {
-    const newCard = itemTemplate.querySelector(".elements__item").cloneNode(true); //клонируем элемент списка из документ-фрагмента
-    newCard.querySelector(".elements__title").textContent = name; //название нового места
-    newCard.querySelector(".elements__photo").src = link; //ссылка на новое место
-    newCard.querySelector('.elements__photo').alt = name; //alt нового места
+    const newCard = itemTemplate.querySelector('.elements__item').cloneNode(true);
+    newCard.querySelector('.elements__title').textContent = name;
+    newCard.querySelector('.elements__photo').src = link;
+    newCard.querySelector('.elements__photo').alt = name;
 
     // событие по клику на удаление - вызов функции удаления карточки
-    newCard.querySelector(".elements__delete").addEventListener('click', () => {
+    newCard.querySelector('.elements__delete').addEventListener('click', () => {
         deleteCard(newCard);
     });
     // добавить клик по картинке 
 
-    newCard.querySelector(".elements__photo").addEventListener('click', () => {
+    newCard.querySelector('.elements__photo').addEventListener('click', () => {
         popupPhotoImg.src = link;
         popupPhotoImg.alt = name;
         popupPhotoTitle.textContent = name;
-
 
         popupPhotoOpen();
     });
 
     // функция лайка по карточке
-    newCard.querySelector(".elements__like").addEventListener('click', function (evt) {
-        evt.target.classList.toggle("elements__like_active");
-    })
+    newCard.querySelector('.elements__like').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('elements__like_active');
+    });
 
     list.prepend(newCard);
 }
@@ -126,18 +123,17 @@ function deleteCard(item) {
 
 // обработчик событий по добавлению новой карточки 
 function createCard(evt) {
-    evt.preventDefault();// Эта строчка отменяет стандартную отправку формы.
+    evt.preventDefault();
 
     addCard(formInputName.value, formInputLink.value); //присваиваем новой карточке значения из полей формы
 
     formInputName.value = '';
     formInputLink.value = '';
 
-    popupPlaceClose();  //закрытие попапа по "Сохранению"
+    popupPlaceClose();
 }
 
 form.addEventListener('submit', createCard);
-
 
 //Перебор массива под существующие функции добавления/удаление/лайки карточки 
 initialCards.forEach(function (item) {
